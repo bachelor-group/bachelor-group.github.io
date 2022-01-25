18e0f4cbcd1dacc3163ae7f4d36c73cd99da9f52
Erlend Bygdås <e.bygdas@stud.uis>
2022-01-25 15:54:13 +0100

    scratch python....

--- bachelor-app/Python/generate_data.py
+++ bachelor-app/Python/generate_data.py
@@ -5,16 +5,35 @@ import os
 # url = "https://storage.googleapis.com/covid19-open-data/v3/google-search-trends.csv"
 url = "~/Downloads/google-search-trends.csv"
 
-search_trends = pd.read_csv(url, iterator=True, chunksize=1000)
+search_trends = pd.read_csv(url, iterator=True, chunksize=10000)
 dict = {}
 
 
+# location = "wwwwwwwwweeeeeeeeeeeeeeeeeeeeeeeeeeha"
 show_header= True
 for chunk in search_trends:
-    location_key = "AU"
-    if not os.path.exists(location_key):
-        os.makedirs(location_key)
-    chunk[chunk.location_key == location_key].to_csv(location_key+".csv", sep=',', index=False, header=show_header)
+    pass
+    # for row in chunk.itertuples(index=False):
+    #     row.to_csv("test.csv", sep=",", header=True)
+        # print(chunk["location_key"])
+    # for row in chunk.location_key:
+    #     print(row)
+    # chuk = chunk.groupby(["location_key"])
+    # for col in chunk:
+    #     print(col)
+        # new_location = str(chunk.location_key)
+        # if new_location != location:
+        #     region_codes = new_location.split("_")
+        #     path = ""
+        #     for region in region_codes:
+        #         path += region+"/"
+        #     print(path)
+        #     if not os.path.exists(path):
+        #         os.makedirs(path)
+        # chunk.location_key.to_csv(path+".csv", sep=',', index=False, header=show_header)
+        # location = "AU"
+        # chunk[chunk.location_key == location].to_csv("test2.csv", sep=',', index=False, header=False)
+        # location = new_location
 
 
 
