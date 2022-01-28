5f6fd1c605a47de07830ab3591f33c9fe501c741
Erlend Bygdås <e.bygdas@stud.uis>
2022-01-28 17:35:25 +0100

    countryselector test wip

---
+++ bachelor-app/src/components/CountrySelector/SelectCountry.test.tsx
@@ -0,0 +1,37 @@
+import { render } from "@testing-library/react";
+import { Tag } from "react-tag-autocomplete";
+import { EpidemiologyData } from "../DataContext/DataTypes";
+import { TagExtended } from "./SelectCountry";
+
+interface Timeouts{
+    type: string,
+    time: number
+    
+}
+const Timeouts: Timeouts[] = [{type: "long", time: 10000}, {type: "short", time: 200}, {type: "medium", time: 5000},]
+
+let countries: string[] = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua & Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia & Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre & Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts & Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', "Timor L'Este", 'Togo', 'Tonga', 'Trinidad & Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks & Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States of America', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe']
+
+//TODO: fix type
+let CountryTag: TagExtended[] = []
+
+let data: TagExtended[] = []
+
+for (let i = 1; 0 < 100; i++) {
+    data.push({ id: i+1, name: countries[i], location_key: CountryTag.location_key[i] })
+}
+
+let FakeLoader = () => {
+    return new Promise<EpidemiologyData[]>((resolve) => {
+        setTimeout(() => (resolve(data)), Timeouts[0].time)
+    })
+}
+
+
+test('renders EpidemiologyPage and checks that correct plots are showing', async () => {
+    // render(<Epidemiology LoadData={FakeLoader} />);
+    // screen.getByRole("progressbar");
+    // let ScatterPlots = await screen.findAllByText("New Cases");
+    // screen.getByText("New Confirmed Cases In Norway")
+    // expect(ScatterPlots.length).toBe(3);
+});
--- bachelor-app/src/components/DataContext/LoadData.tsx
+++ bachelor-app/src/components/DataContext/LoadData.tsx
@@ -11,8 +11,7 @@ const url = "https://storage.googleapis.com/covid19-open-data/v3/location/"
 export const LoadData = (requestedCountries: TagExtended[], loadedCountries: TagExtended[], data: EpidemiologyData[]) => {
     return new Promise<EpidemiologyData[]>((resolve) => {
         let loaded_csv: number = loadedCountries.length;
-        if (requestedCountries.length > loadedCountries.length) {
-
+        if (requestedCountries.length + 1 === loadedCountries.length) {
             csv(url + requestedCountries.at(-1)!.location_key + ".csv").then(d => {
                 d.forEach(element => {
                     data.push(element)
