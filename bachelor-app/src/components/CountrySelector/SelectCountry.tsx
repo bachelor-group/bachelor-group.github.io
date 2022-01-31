b53b80bb7da937a593f91ed1b39e95007f562fce
Erlend Bygdås <e.bygdas@stud.uis>
2022-01-31 16:52:40 +0100

    cleanup

--- bachelor-app/src/components/CountrySelector/SelectCountry.tsx
+++ bachelor-app/src/components/CountrySelector/SelectCountry.tsx
@@ -1,7 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import ReactTags, { Tag } from 'react-tag-autocomplete'
-import { iso31661 } from 'iso-3166'
-import { EpidemiologyData } from "../DataContext/DataTypes";
 import { csv } from "d3";
 
 
@@ -16,7 +14,7 @@ const LoadData = () => {
     return new Promise<TagExtended[]>((resolve) => {
         let Data: TagExtended[] = []
         csv(url).then(d => {
-            d.forEach((element, index) => {
+            d.forEach((element) => {
                 if (element.location_key === element.country_code) {
                     Data.push({ id: Data.length, name: element.country_name!, location_key: element.country_code! })
                 }
@@ -30,27 +28,22 @@ const LoadData = () => {
 export const SelectCountry = ({ selectedCountries }: { selectedCountries: (countries: TagExtended[]) => void }) => {
 
     const [tags, setTags] = useState<TagExtended[]>([])
-    const [allCountries, setAllCountries] = useState<Tag[]>([])
+    const [suggestions, setSuggestions] = useState<Tag[]>([])
     const [data, setData] = useState<TagExtended[]>([])
     const reactTags = useRef<Tag>()
 
     useEffect(() => {
         LoadData().then((d: TagExtended[]) => {
             setData(d)
+            setSuggestions(d)
         })
     }, [])
 
-    // remove in future if we should also display regions of country
-    // just set suggestion list to data
+
+
+    // when tags change, let parent component know
     useEffect(() => {
-        // setAllCountries(data.filter((v, i, a) => a.findIndex(t => (t.name === v.name)) === i))
-        setAllCountries(data)
-
-    }, [data])
-
-
-    useEffect(() => {
-        // emit to parent a new country is added:
+        // tags does not include location_key, data does:
         selectedCountries(data.filter(d => tags.find(t => t.id === d.id)))
     }, [tags])
 
@@ -58,26 +51,21 @@ export const SelectCountry = ({ selectedCountries }: { selectedCountries: (count
     const onDelete = useCallback((tagIndex) => {
         if (tagIndex !== -1) {
             const deletedTag = tags.filter((_, i) => i === tagIndex)
-            // bug here? with indexing? be aware!
             setTags(tags.filter((_, i) => i !== tagIndex))
 
-            // on remove, need to add back to allCountries
-            setAllCountries([...allCountries, deletedTag[0]])
-
+            // on remove, need to add back to suggestions
+            setSuggestions([...suggestions, deletedTag[0]])
         }
-    }, [tags, allCountries])
+    }, [tags, suggestions])
 
 
-    // newTag er bare en Tag, så får ikke med landskoden... om vi ikk efår det er det nok ikke vits å laste inn data, og så filtrere bort regions?
-    // id'ene stemmer overens, så mulig vi må bare sjekke med datalisten
     const onAddition = useCallback((newTag) => {
         setTags([...tags, newTag])
 
         // remove from suggestion list:
-        setAllCountries(allCountries.filter(Tag => Tag.name !== newTag.name))
+        setSuggestions(suggestions.filter(Tag => Tag.name !== newTag.name))
 
-    }, [tags, allCountries])
-    
+    }, [tags, suggestions])
 
     return (
         <>
@@ -86,7 +74,7 @@ export const SelectCountry = ({ selectedCountries }: { selectedCountries: (count
                 //@ts-ignore
                 ref={reactTags}
                 tags={tags}
-                suggestions={allCountries}
+                suggestions={suggestions}
                 onDelete={onDelete}
                 onAddition={onAddition}
                 minQueryLength={0}
