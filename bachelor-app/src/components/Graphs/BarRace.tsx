36fcd4e412e9898fedf6e21ca034b1fdaa0a5e5f
Stian <Stian.ba@hotmail.com>
2022-02-07 17:25:12 +0100

    BarRace, change number of bars visible

--- bachelor-app/src/components/Graphs/BarRace.tsx
+++ bachelor-app/src/components/Graphs/BarRace.tsx
@@ -12,7 +12,6 @@ interface BarRaceProps {
 }
 
 const MARGIN = { top: 50, right: 20, bottom: 0, left: 20 };
-let top_n = 12;
 
 type Bar = {
     Data: DataType,
@@ -25,6 +24,9 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
     const titleRef = useRef(null);
     const boundsRef = useRef(null);
     const svgRef = useRef(null);
+
+    //Number of Bars
+    const [top_n, setTop_n] = useState(12)
 
     //Bounds
     const boundsWidth = Width - MARGIN.right - MARGIN.left - 0.5 * MARGIN.left;
@@ -134,7 +136,7 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
     // Y axis
     const yScale = useMemo(() => {
         return scaleLinear().domain([top_n, 0]).range([boundsHeight, 0]);
-    }, [barsData, boundsHeight]);
+    }, [barsData, boundsHeight, top_n]);
 
     // X axis
     let xScale = useMemo(() => {
@@ -167,10 +169,12 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
 
     useEffect(() => {
         if (barsData.length !== 0) updatePlot();
-    }, [startDate])
+    }, [startDate, top_n])
 
+    //Initial draw
     function drawPlot(cursor = FindDateIndex(startDate)) {
-        let transitionLength = 500;
+        let transitionLength = 250;
+        let delay = 80;
         let currentSlice = barsData[cursor].sorted.slice(0, top_n)
         // @ts-ignore
         let bars = select(boundsRef.current).selectAll("rect").data(currentSlice, d => d.property);
@@ -188,7 +192,7 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
             .duration(transitionLength)
             .ease(easeLinear)
             .attr("width", d => xScale(d.value))
-            .delay(function (d, i) { return (i * 100) });
+            .delay(function (d, i) { return (i * delay) });
 
         // @ts-ignore
         let labels = select(boundsRef.current).selectAll('.label').data(currentSlice, d => d.property);
@@ -198,15 +202,16 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
             .append('text')
             .attr("class", 'label')
             .attr("x", d => xScale(d.value) - 8)
-            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2 + 5 - 20)
+            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2 - 20)
             .attr('text-anchor', 'end')
+            .attr("alignment-baseline", "middle")
             .html(d => d.property.slice(14).replace("_", " "))
             .attr("opacity", 0)
             .transition()
-            .duration(transitionLength/10)
+            .duration(transitionLength / 10)
             .ease(easeLinear)
-            .delay(function(d,i) {return transitionLength + (i)* 100})
-            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2 + 5)
+            .delay(function (d, i) { return transitionLength + (i) * delay })
+            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2)
             .attr("opacity", 1);
 
         // @ts-ignore
@@ -216,14 +221,15 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
             .append('text')
             .attr("class", 'valueLabel')
             .attr("x", d => xScale(d.value) + 5)
-            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2 + 5 - 20)
+            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2 - 20)
+            .attr("alignment-baseline", "middle")
             .attr("opacity", 0)
             .text(d => format(',.2f')(d.value))
             .transition()
             .duration(transitionLength / 10)
             .ease(easeLinear)
-            .delay(function(d,i) {return transitionLength + (i)* 100})
-            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2 + 5)
+            .delay(function (d, i) { return transitionLength + (i) * delay })
+            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2)
             .attr("opacity", 1);
     }
 
@@ -255,30 +261,33 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
             .enter()
             .append('rect')
             .attr("x", xScale(0) + 1)
-            .attr("y", d => yScale(top_n) + 5)
+            .attr("y", d => yScale(top_n))
             .attr("height", yScale(1) - yScale(0) - barPadding)
             .style("fill", (d) => d.colour.toString())
             .style("stroke", "black")
             .style("stroke-width", "1px")
-            .attr("width", d => xScale(d.value))
+            .attr("width", d => xScale(currentSlice[top_n - 1]["value"]))
             .transition()
             .duration(tickDuration)
             .ease(easeLinear)
             .attr("width", d => xScale(d.value))
-            .attr("y", d => yScale(d.rank) + 5);
+            .attr("height", yScale(1) - yScale(0) - barPadding)
+            .attr("y", d => yScale(d.rank) + barPadding / 2);
 
         bars
             .transition()
             .duration(tickDuration)
             .ease(easeLinear)
             .attr("width", d => xScale(d.value))
-            .attr("y", d => yScale(d.rank) + 5);
+            .attr("height", yScale(1) - yScale(0) - barPadding)
+            .attr("y", d => yScale(d.rank) + barPadding / 2);
 
         bars
             .exit()
             .transition()
             .duration(tickDuration)
             .ease(easeLinear)
+            .attr("width", d => xScale(currentSlice[top_n - 1]["value"]))
             .attr("y", yScale(top_n) + 10)
             .remove();
 
@@ -289,14 +298,16 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
             .enter()
             .append('text')
             .attr("class", 'label')
-            .attr("x", d => xScale(d.value) - 8)
-            .attr("y", d => yScale(top_n) + (yScale(1) - yScale(0)) / 2 + 5)
+            .attr("x", d => xScale(currentSlice[top_n - 1]["value"]))
+            .attr("y", d => yScale(top_n) + (yScale(1) - yScale(0)) / 2)
             .attr('text-anchor', 'end')
+            .attr("alignment-baseline", "middle")
             .html(d => d.property.slice(14).replace("_", " "))
             .transition()
             .duration(tickDuration)
             .ease(easeLinear)
-            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2 + 5)
+            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2)
+            .attr("x", d => xScale(d.value) - 8)
 
 
         labels
@@ -304,7 +315,7 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
             .duration(tickDuration)
             .ease(easeLinear)
             .attr("x", d => xScale(d.value) - 8)
-            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2 + 5);
+            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2);
 
         labels
             .exit()
@@ -312,8 +323,8 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
             .duration(tickDuration)
             .ease(easeLinear)
             //@ts-ignore
-            .attr("x", d => xScale(d.value) - 8)
-            .attr("y", d => yScale(top_n) + (yScale(1) - yScale(0)) / 2 + 5)
+            .attr("x", d => xScale(currentSlice[top_n - 1]["value"]) - 8)
+            .attr("y", d => yScale(top_n) + (yScale(1) - yScale(0)) / 2)
             .remove();
 
         // @ts-ignore
@@ -323,12 +334,15 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
             .enter()
             .append('text')
             .attr("class", 'valueLabel')
-            .attr("x", d => xScale(d.value) + 5)
-            .attr("y", d => yScale(top_n) + (yScale(1) - yScale(0)) / 2 + 5)
+            .attr("x", d => xScale(currentSlice[top_n - 1]["value"]))
+            .attr("y", d => yScale(top_n) + (yScale(1) - yScale(0)) / 2)
+            .attr("alignment-baseline", "middle")
             .text(d => format(',.2f')(d.value))
             .transition()
             .duration(tickDuration)
-            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2 + 5)
+            .ease(easeLinear)
+            .attr("x", d => xScale(d.value) + 5)
+            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2)
 
 
         valueLabels
@@ -342,14 +356,15 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
                 };
             })
             .attr("x", d => xScale(d.value) + 5)
-            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2 + 5);
+            .attr("y", d => yScale(d.rank) + (yScale(1) - yScale(0)) / 2);
 
         valueLabels
             .exit()
             .transition()
             .duration(tickDuration)
             .ease(easeLinear)
-            .attr("y", d => yScale(top_n) + (yScale(1) - yScale(0)) / 2 + 5)
+            .attr("x", d => xScale(currentSlice[top_n - 1]["value"]))
+            .attr("y", d => yScale(top_n) + (yScale(1) - yScale(0)) / 2)
             .remove();
     }
 
@@ -360,7 +375,6 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
             setTickDuration(parseInt(event.target.value));
         }
     }
-
 
     // Date Handling
     let maxDate = ""
@@ -385,6 +399,13 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
         }
     }
 
+    function handleNumberBarsChange(event: React.ChangeEvent<HTMLInputElement>) {
+        if (ticker === undefined) {
+            console.log(event.target.value)
+            setTop_n(parseInt(event.target.value));
+        }
+    }
+
     return (
         <div style={{
             display: "flex", flexDirection: "column", alignItems: "center", border: "none"
@@ -402,6 +423,12 @@ function BarRace({ Width, Height, Plot }: BarRaceProps) {
                                 <label htmlFor="startDate">Start Date:</label>
                                 <input type="date" name="startDate" id="" min={minDate} max={maxDate} value={startDate}
                                     onChange={(e) => handleDateChange(e)} />
+                            </div>
+
+                            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                                <label htmlFor="numberBars">Number of Bars:</label>
+                                <input type="number" name="numberBars" id="" min={1} max={50} value={top_n}
+                                    onChange={(e) => handleNumberBarsChange(e)} />
                             </div>
 
                             <Button onClick={() => Animate()}>{ticker === undefined ? "Start me" : "Stop me"}</Button>
