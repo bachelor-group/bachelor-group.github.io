5823588bbedfa4d2336345a4b4522e179208a736
ebygd <e.bygdas@stud.uis>
2022-03-15 14:37:55 +0100

    added comments, fixed extreme inefficiency

--- bachelor-app/Python/fetch_data.py
+++ bachelor-app/Python/fetch_data.py
@@ -8,23 +8,22 @@ epidemiology = pd.read_csv("https://storage.googleapis.com/covid19-open-data/v3/
 index_url = "https://storage.googleapis.com/covid19-open-data/v3/index.csv"
 
 
+# fetch only specific columns from a csv file:
 def fetch_data_columns(filename: str, dt: str, cols):
     path = "public/csvData/"+filename
-
     # Create the file if it does not exist
     if not os.path.isfile(path):
         open(path, 'w+').close()
     dt.to_csv(path, index=False, columns=cols)
 
 
+# Generates a file with date: total_confirmed
 def date_total_confirmed():
         HistogramData = dict()
-        with open("public/csvData/epidemiology_min.csv", 'r') as csvfile:
+        with open("public/csvData/cases.csv", 'r') as csvfile:
             datareader = csv.reader(csvfile)
-
             # skip header
             next(datareader)
-
             try:
                 for row in datareader:
                     if row[0] in HistogramData:
@@ -33,14 +32,13 @@ def date_total_confirmed():
                     else:
                         if not math.isnan(float(row[2])):
                             HistogramData[row[0]] = float(row[2])
-
             except ValueError as e:
                 print("error: ", e)
-        
         with open("public/csvData/total_confirmed.csv", 'w') as csvfile:
             csvfile.write("%s,%s\n" % ("date", "total_confirmed"))
             for key in HistogramData.keys():
                 csvfile.write("%s,%s\n" % (key, HistogramData[key]))
+
 
 # STÅ I /bachelor-app
 if __name__=="__main__":
@@ -48,8 +46,10 @@ if __name__=="__main__":
     # search_trends = pd.read_csv("https://storage.googleapis.com/covid19-open-data/v3/google-search-trends.csv")
     # vaccinations = pd.cread_csv("https://storage.googleapis.com/covid19-open-data/v3/vaccinations.csv")
 
-    fetch_data_columns("cases.csv", epidemiology, ["date", "location_key", "new_confirmed"])
     fetch_data_columns("index_min.csv", pd.read_csv(index_url), ["location_key", "country_code", "subregion1_code", "subregion2_code"])
+
+    # update cases.csv before date total_confirmed to get newest version
+    fetch_data_columns("cases.csv", epidemiology, ["date", "location_key", "new_confirmed"])
     date_total_confirmed()
     
     
--- bachelor-app/Python/generate_data.py
+++ bachelor-app/Python/generate_data.py
@@ -1,11 +1,8 @@
 import csv
-from distutils.log import error
 import math
 import pandas as pd
 import os
 from fetch_data import fetch_data_columns
-
-
 
 
 def epidemiology_locations(location, path):
@@ -16,34 +13,51 @@ def epidemiology_locations(location, path):
     
 
 def write_to_file(region_code, filename):
+    print("=================== "+region_code+" ====================")
     path="public/csvData/aggregated"
+    print("fetching")
+
     dt = pd.read_csv("https://storage.googleapis.com/covid19-open-data/v3/location/"+region_code+".csv")
+    print("done fetching")
+
     region_code = region_code.split("_")
     for region in region_code:
         path += "/"+region
     if not os.path.exists(path):
         os.makedirs(path)
+    print("Writing")
     dt.to_csv(path+"/"+filename+".csv", index=False, columns=COLS)
+    print("Done writing")
+    print()
+    
     
     
 def generate_data():
-    with open("public/csvData/index_min_WIP.csv", 'r') as csvfile:
+    with open("public/csvData/index_min.csv", 'r') as csvfile:
         datareader = csv.reader(csvfile)
         
         # skip header
         next(datareader)
 
         for row in datareader:
-            print(row[0].split("_"))
+            # print(row[0].split("_"))
 
             region_codes = row[0].split("_")
-
+            
+            url=""
+            for i,region in enumerate(region_codes):
+                url += region
+                if i != len(region_codes)-1:
+                    url += "_"
+            # print(url)
             try:
-                write_to_file(region_codes[0], "cases")
-                write_to_file(region_codes[0]+"_"+region_codes[1], "cases")
-                write_to_file(region_codes[0]+"_"+region_codes[1]+"_"+region_codes[2], "cases")
+                write_to_file(url, "cases")
+
+                # write_to_file(region_codes[0], "cases")
+                # write_to_file(region_codes[0]+"_"+region_codes[1], "cases")
+                # write_to_file(region_codes[0]+"_"+region_codes[1]+"_"+region_codes[2], "cases")
             except:
-                print()
+                print("error")
 
 
 
