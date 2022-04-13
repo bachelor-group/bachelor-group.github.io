e5f0536b132f2610fd70c73a3aaf3450f0047507
Stian <stian.ba@hotmail.com>
2022-04-13 20:11:26 +0200

    Added spacing every 3rd digit in tooltips

--- bachelor-app/src/components/Graphs/Tooltip.tsx
+++ bachelor-app/src/components/Graphs/Tooltip.tsx
@@ -8,14 +8,14 @@ export const GraphTooltip = (Plot: Plot, Data: DataType, Content: JSX.Element) =
     return (
         <OverlayTrigger placement="auto" overlay={
             <Popover id="popover-contained">
-                {SHOWCOUNTRY ? <Popover.Header as="h3">Country Text</Popover.Header> : <></>
+                {SHOWCOUNTRY ? <Popover.Header as="h3">{Data["country_name"]} at {Data["date"]}</Popover.Header> : <></>
 
                 }
 
                 <Popover.Body>
-                    <strong>{Plot.Axis[0].replace("_", " ")}:</strong> {Data[Plot.Axis[0]]}
+                    <strong>{Plot.Axis[0].replace("_", " ")}:</strong> {Data[Plot.Axis[0]]!.replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
                     <br />
-                    <strong>{Plot.Axis[1].replace("_", " ")}:</strong> {Data[Plot.Axis[1]]}
+                    <strong>{Plot.Axis[1].replace("_", " ")}:</strong> {Data[Plot.Axis[1]]!.replace(/\B(?=(\d{3})+(?!\d))/g, " ")}
                 </Popover.Body>
             </Popover>
         }>
