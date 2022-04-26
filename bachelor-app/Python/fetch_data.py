e707b71e134a07846faba2a8c084c14fd3db9238
ebygd <e.bygdas@stud.uis>
2022-04-26 14:21:46 +0200

    histogram data auto downloads from bucket and deletes locally after completion

--- bachelor-app/Python/fetch_data.py
+++ bachelor-app/Python/fetch_data.py
@@ -33,9 +33,22 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
     print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
 
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        "Downloaded storage object {} from bucket {} to local file {}.".format(
+            source_blob_name, bucket_name, destination_file_name
+        )
+    )
 
 # Generates a file with date: total_confirmed
 def date_total_confirmed(datatype):
+    download_blob("covid-data-minimized", datatype+".csv", "public/csvData/"+datatype+".csv")
     HistogramData = dict()
     with open("public/csvData/" + datatype + ".csv", "r") as csvfile:
         datareader = csv.reader(csvfile)
@@ -64,6 +77,10 @@ def date_total_confirmed(datatype):
 
     # Upload the file to the cloud:
     upload_blob("covid-data-minimized","public/csvData/"+datatype+"_total.csv", datatype+"_total.csv")
+    
+    # files are no longer needed, delete them:
+    os.remove("public/csvData/"+datatype+"_total.csv")
+    os.remove("public/csvData/"+datatype+".csv")
 
 
 # STÅ I /bachelor-app
