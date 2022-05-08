ca7fc979664aa428a11611cdc7aff5e7a4c8768b
Stian <stian.ba@hotmail.com>
2022-05-08 12:33:53 +0200

    Changed SelectCountry to be a single component in page and handle logic internally

--- bachelor-app/src/components/GraphPage/GraphPage.tsx
+++ bachelor-app/src/components/GraphPage/GraphPage.tsx
@@ -1,4 +1,4 @@
-import { Children, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { csv } from "d3";
 import { Nav } from 'react-bootstrap';
 import Tab from 'react-bootstrap/esm/Tab';
@@ -9,6 +9,7 @@ import Epidemiology from '../EpidemiologyContext/Epidemiology';
 import SearchTrends from '../SearchTrends/SearchTrends';
 import Vaccinations from '../Vaccinations/Vaccinations';
 import CustomGraphs from './CustomGraphs';
+import { Tag } from 'react-tag-autocomplete';
 
 
 interface Props {
@@ -23,7 +24,7 @@ export const GraphPage = ({ LoadDataAsMap = _LoadDataAsMap, LoadIndex = _LoadInd
     const [key, setKey] = useState<string>('epidemiology');
 
     const [mapData, setMapData] = useState<Map<string, DataType[]>>(new Map());
-    const [indexMap, setIndexMap] = useState<Map<string, IMap>>(new Map());
+    const [indexMap, setIndexMap] = useState<Map<number, Map<string, IMap>>>(new Map());
     const [LoadedCountries, setLoadedCountries] = useState<TagExtended[]>([]);
     const [SelectedLocations, setSelectedLocations] = useState<TagExtended[]>([]);
 
@@ -50,7 +51,6 @@ export const GraphPage = ({ LoadDataAsMap = _LoadDataAsMap, LoadIndex = _LoadInd
     useEffect(() => {
         LoadIndex().then((d) => {
             setIndexMap(d);
-
         })
     }, [])
 
@@ -58,9 +58,7 @@ export const GraphPage = ({ LoadDataAsMap = _LoadDataAsMap, LoadIndex = _LoadInd
 
     return (
         <>
-            <SelectCountry SelectedRegions={(countries: TagExtended[])=>setSelectedLocations(countries)} SelectedLocations={SelectedLocations} Key={key} ADMINLVL={0} AllLocations={indexMap} />
-            <SelectCountry SelectedRegions={(countries: TagExtended[])=>setSelectedLocations(countries)} SelectedLocations={SelectedLocations} Key={key} ADMINLVL={1} AllLocations={indexMap} />
-            <SelectCountry SelectedRegions={(countries: TagExtended[])=>setSelectedLocations(countries)} SelectedLocations={SelectedLocations} Key={key} ADMINLVL={2} AllLocations={indexMap} />
+            <SelectCountry SelectedRegions={(countries: TagExtended[]) => setSelectedLocations(countries)} Key={key} AllLocations={indexMap} />
 
             <br></br>
 
@@ -118,7 +116,9 @@ export const GraphPage = ({ LoadDataAsMap = _LoadDataAsMap, LoadIndex = _LoadInd
 
 
 
-export interface IMap {
+export interface IMap extends Tag {
+    id: number,
+    locationKey: string,
     children: string[]
     name: string,
 
@@ -127,23 +127,31 @@ export interface IMap {
 const url = "https://storage.googleapis.com/covid19-open-data/v3/index.csv"
 
 export const _LoadIndex = () => {
-    return new Promise<Map<string, IMap>>((resolve) => {
-        let IndexMap = new Map<string, IMap>();
+    return new Promise<Map<number, Map<string, IMap>>>((resolve) => {
+        let IndexMap = new Map<number, Map<string, IMap>>();
         csv(url).then(d => {
-            d.forEach((element) => {
-                let keyLength = element.location_key!.split("_").length
+            d.forEach((element, i) => {
+                let adminLvl = element.location_key!.split("_").length - 1;
+
+                //find name
                 let name = element.country_name;
-                if (keyLength !== 1){
-                    name = element[`subregion${keyLength-1}_name`]
+                if (adminLvl !== 0) {
+                    name = element[`subregion${adminLvl}_name`]
                 }
 
-                IndexMap.set(element.location_key!, { children: [], name: name! })
+
+                if (!IndexMap.has(adminLvl)) {
+                    IndexMap.set(adminLvl, new Map())
+                }
+
+                IndexMap.get(adminLvl)!.set(element.location_key!, { id: i, locationKey: element.location_key!, name: name!, children: [] })
 
                 //  find parent element, and add self to children of parent 
                 let parent: string[] = element.location_key!.split("_").slice(0, -1);
                 if (parent.length > 0) {
+                    let parentLvl = adminLvl - 1;
                     let parentKey = parent.join("_");
-                    let parentEntry = IndexMap.get(parentKey)!;
+                    let parentEntry = IndexMap.get(parentLvl)!.get(parentKey)!;
                     parentEntry.children.push(element.location_key!)
                 }
 
